2241d31ad7235d3e589396fad0c753341cf09d97
Tabytac <45774869+tabytac@users.noreply.github.com>
2026-02-08 14:38:24 +0000

    Initialize Fabric mod

---
+++ src/client/kotlin/wynnwarcooldown/tabytac/WynnWarCooldownClient.kt
@@ -0,0 +1,9 @@
+package wynnwarcooldown.tabytac
+
+import net.fabricmc.api.ClientModInitializer
+
+object WynnWarCooldownClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
