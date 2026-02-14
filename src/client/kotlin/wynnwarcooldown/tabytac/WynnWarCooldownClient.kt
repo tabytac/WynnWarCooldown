69dc1514e5bc4c03699a8a012604e1c585644c06
Tabytac <45774869+tabytac@users.noreply.github.com>
2026-02-14 13:57:53 +0000

    Add toggle keybind for Timer HUD

--- src/client/kotlin/wynnwarcooldown/tabytac/WynnWarCooldownClient.kt
+++ src/client/kotlin/wynnwarcooldown/tabytac/WynnWarCooldownClient.kt
@@ -1,10 +1,17 @@
 package wynnwarcooldown.tabytac
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
 
 object WynnWarCooldownClient : ClientModInitializer {
+	private lateinit var toggleHudKeyBinding: KeyBinding
+
 	override fun onInitializeClient() {
 		// Load config from file
 		ModConfig.load()
@@ -14,6 +21,26 @@ object WynnWarCooldownClient : ClientModInitializer {
 
 		// Register client commands
 		CommandManager.registerCommands()
+
+		// Register keybind for toggling HUD
+		toggleHudKeyBinding = KeyBindingHelper.registerKeyBinding(
+			KeyBinding(
+				"key.wynn-war-cooldown.toggle_hud",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_COMMA, // Default key: Comma
+				"category.wynn-war-cooldown"
+			)
+		)
+
+		// Register keybind handler
+		ClientTickEvents.END_CLIENT_TICK.register { client ->
+			while (toggleHudKeyBinding.wasPressed()) {
+				ModConfig.showTimerHud = !ModConfig.showTimerHud
+				ModConfig.save()
+				val status = if (ModConfig.showTimerHud) "visible" else "hidden"
+				client.inGameHud?.chatHud?.addMessage(net.minecraft.text.Text.literal("§aTimer HUD is now $status"))
+			}
+		}
 
 		// Register HUD render callback (deprecated API, suppressed)
 		@Suppress("DEPRECATION")
